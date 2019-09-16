8fe9602e318155c6c7913d50435122c5f68d3057
mitsune91 <43758008+mitsune91@users.noreply.github.com>
2019-09-16 16:34:41 +0200

    Added Chapter 3 (both my way and the correction) and Chapter 4 WIP

--- Chapitre2/Queue.ts
+++ Chapitre2/Queue.ts
@@ -1,4 +1,4 @@
-class Queue<T> {
+class Queue<T> { //classe d'objet à typer à l'instanciation
 
     private _attr: T[];
 
@@ -35,3 +35,44 @@ queue.push(2);
 queue.push(3);
 queue.push(4);
 console.log(queue.pop());
+
+/* La correction
+
+
+class Queue<T> {
+    private _elems: T[] = [];
+
+    push(elem: T): void {
+        this._elems.push(elem);
+    }
+
+    pop(): T {
+        if(this._elems)
+            return this._elems.shift();
+
+        throw "empty queue";
+    }
+
+    // getter
+    get elem(): T[] { return this._elems; }
+}
+
+let queue = new Queue<number>();
+queue.push(1);
+queue.push(2);
+queue.push(3);
+queue.push(4);
+console.log(queue.pop()); // affiche 1
+
+// Vous pouvez définir un type Array<number> pour T
+let queueArray = new Queue<Array<number>>();
+queueArray.push([1, 2]);
+queueArray.push([3, 4]);
+queueArray.push([5, 6]);
+queueArray.push([7, 8]);
+
+// produit une erreur car le type n'est pas respecté
+// queueArray.push("elem");
+console.log(queueArray.pop()); // affiche [1,2]
+
+ */
